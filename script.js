4d3e5d2b804782f21e5c7a9ce691fc2275bdb3dc
Akash Holambe <akashholambe1999@gmail.com>
2024-01-02 12:38:05 +0530

    Basic concept about Javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello Akash..");
+age = 23;
+console.log(age);
+
+// Variable which start form number is not allowed and also spacing is also not allowed.
+
+var Variable =
+  "This variable have global scope so it will get accessed verywhere.";
+
+// let and cont have local scope
+
+if (true) {
+  let a = 20;
+}
+console.log(a); // so here it give error in consloe so it prove that it dont have access outside the barcket.
+
+const variable =
+  "This have fixed value not we cant not change its value this is fixed now";
---
+++ script2.js
@@ -0,0 +1,22 @@
+let num = 3.14;
+let num2 = 10;
+
+console.log(num + num2);
+
+const person = {
+  firstName: "sagar",
+  lastName: "Holambe",
+  age: 19,
+  isLoggedin: "No",
+};
+console.log(person);
+
+// in js true = 1; and false = 0;
+
+let string = "1" + "1";
+console.log(string);
+
+
+
+string * 1;
+console.log(string)
