778cc14b5393d8ec717e77660172f55b5e7c69ef
Akash Holambe <akashholambe1999@gmail.com>
2024-01-07 23:54:13 +0530

    pratice question 2

--- script3.js
+++ script3.js
@@ -27,12 +27,32 @@ if (a == 10) {
 }
 
 // === equal to check for stricketly for values and datatypes also.
-//var age = prompt(age);
+//var age = prompt("Enter age");
+let age = 23;
 if (age == 23) {
   console.log("age is 23");
 } else "You are less than or grater than 23";
 
-const number = prompt(number);
-if (number % 5 == 0) {
-  console.log("The number is multiple of 5");
-} else "The number is NOT multiple of 5";
+console.log("Enter a number :");
+const number = prompt("Enter a number");
+
+if (number % 5 === 0) {
+  console.log(number, "The number is a multiple of 5");
+} else {
+  console.log(number, "The number is NOT a multiple of 5");
+}
+
+// Pratice question 2
+
+let marks = prompt("Enter marks");
+if (marks >= 90 && marks <= 100) {
+  console.log(marks, "A");
+} else if (marks >= 70 && marks <= 89) {
+  console.log(marks, "B");
+} else if (marks >= 60 && marks <= 69) {
+  console.log(marks, "C");
+} else if (marks >= 50 && marks <= 59) {
+  console.log(marks, "D");
+} else if (marks >= 0 && marks <= 49) {
+  console.log(marks, "F");
+}
