b7c908dacf5f07e49595cd856fcb1ba536d67022
Akash Holambe <akashholambe1999@gmail.com>
2024-01-07 23:15:05 +0530

    Operators

--- script3.js
+++ script3.js
@@ -1,6 +1,19 @@
-//  Creating function in js
+//  Using operator in java..
+// Arithmetic operator +,-,/,%.
 
-function name() {
-  console.log("AKsah Holambe");
-}
-name();
+a = 10;
+b = 20;
+c = a + b;
+console.log(c);
+
+d = a * b;
+console.log(d);
+e = a % b;
+console.log(e);
+
+// Increment and decrement these are urinary operator.
+
+a++;
+console.log(a);
+a--;
+console.log(a);
