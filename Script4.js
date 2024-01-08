239d047b83827d2b9c21f1d4e87e072dbf424707
Akash Holambe <akashholambe1999@gmail.com>
2024-01-08 20:21:11 +0530

    Loop in js

---
+++ Script4.js
@@ -0,0 +1,17 @@
+// // Loops in js "Loop is used to execute something repeat and reapeat"
+
+// for (let i = 1; i < 5; i++) {
+//   console.log("Akash Holambe", i);
+// }
+
+// i = 1;
+// do {
+//   console.log("Sagar Holambe", i);
+//   i++;
+// } while (i <= 10);
+
+i = 1;
+while (i <= 10) {
+  console.log("Holambe", i * 2);
+  i++;
+}
