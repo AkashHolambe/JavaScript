7bc7e2df7a34196ba233d13c68043e5dc1113a3d
Akash Holambe <akashholambe1999@gmail.com>
2024-01-12 21:44:24 +0530

    Dom Quries

---
+++ script5.js
@@ -0,0 +1,10 @@
+// Here we learning DOM which means we access our HTML code using js.
+let id = document.getElementById("Akash");
+console.dir(id);
+console.log(id);
+
+let button = document.getElementsByClassName("beast-button");
+console.dir(button);
+console.log(button);
+
+
