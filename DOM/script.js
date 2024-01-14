80aa495f704c12089003f6f4b5a50fef09062a73
Akash Holambe <akashholambe1999@gmail.com>
2024-01-14 19:51:44 +0530

    DOM learning js

---
+++ DOM/script.js
@@ -0,0 +1,15 @@
+console.log(document.documentElement);
+// above is document object js
+
+console.log(document.body);
+// it prints body of html code
+
+console.log(document.head);
+// prints head of html code
+
+console.log(document.childNodes);
+console.log(document.children);
+
+
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
