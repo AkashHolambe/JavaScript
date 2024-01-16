3b46915372e1c765dbd47e064a39b183139935bf
Akash Holambe <akashholambe1999@gmail.com>
2024-01-16 20:17:52 +0530

    Dom2

--- DOM/script.js
+++ DOM/script.js
@@ -13,3 +13,5 @@ console.log(document.children);
 
 console.log(document.body.firstChild);
 console.log(document.body.lastChild);
+
+
---
+++ DOM/script2.js
@@ -0,0 +1,9 @@
+document.getElementById("mainpara").innerHTML = "Hello JavaScript";
+
+// Now in p tag we changed the inner html to hello javascript
+
+document.getElementById("mainpara").style.fontSize = "35px";
+document.getElementById("mainpara").style.color = "red";
+document.getElementById("mainpara").style.backgroundColor = "green";
+
+// here we applied css by using js
