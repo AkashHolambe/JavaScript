c1a3c2ef8d0efff19215697adc3439db9be0a1b8
Akash Holambe <akashholambe1999@gmail.com>
2024-01-16 20:52:54 +0530

    fun page

--- DOM/script2.js
+++ DOM/script2.js
@@ -7,3 +7,17 @@ document.getElementById("mainpara").style.color = "red";
 document.getElementById("mainpara").style.backgroundColor = "green";
 
 // here we applied css by using js
+
+document.getElementById("mainpara").style.display = "none";
+
+// js can hide our elements
+
+document.getElementById("mainpara").style.display = "Block";
+// js can show  our elements
+
+function myfun() {
+  document.getElementById("heading").innerHTML = "Akash Holambe";
+}
+
+document.write(); //  by using this we can write any thing in our html code
+// but The document.write() method should only be used for testing.
